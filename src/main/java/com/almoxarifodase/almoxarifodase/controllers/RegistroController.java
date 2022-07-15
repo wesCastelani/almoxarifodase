cb67116646b470c770e8f7d2e2dc31e4f12ac1af
Wesley Castelani <wesleycastelani96@gmail.com>
2022-07-14 21:34:02 -0300

    fiz as classes separadas para registros e fiz a gravacao dos registros em DB (:

---
+++ src/main/java/com/almoxarifodase/almoxarifodase/controllers/RegistroController.java
@@ -0,0 +1,2 @@
+package com.almoxarifodase.almoxarifodase.controllers;public class RegistroController {
+}
---
+++ src/main/java/com/almoxarifodase/almoxarifodase/service/RegistroService.java
@@ -0,0 +1,2 @@
+package com.almoxarifodase.almoxarifodase.service;public class RegistroService {
+}
